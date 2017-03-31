da628572e0ab8f0cabd8f5bee43789949241758e
Ihar Yudziankou <Ihar_Yudziankou@epam.com>
2017-03-31 12:00:25 +0300

    decorator init

---
+++ src/main/java/com/headfirstlabs/hfdp/decorator/Beverage.java
@@ -0,0 +1,13 @@
+package com.headfirstlabs.hfdp.decorator;
+
+/**
+ * Created by Администратор on 30.03.2017.
+ */
+public class Beverage {
+    String description;
+
+    private 
+    public double cost() {
+        return 0D;
+    }
+}
